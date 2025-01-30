10d9a0d14d149d0079678e955d313c4a4c4dbed2
Prof Kit Barry <kit.barry@sheridancollege.ca>
2025-01-30 14:40:47 -0500

    Made some noise

--- Assets/Scripts/EnableDisable.cs
+++ Assets/Scripts/EnableDisable.cs
@@ -7,6 +7,8 @@ public class EnableDisable : MonoBehaviour
     public SpriteRenderer sr;
     public EnableDisable script;
     public GameObject go;
+    public AudioSource audioSource;
+    public AudioClip clip;
     void Update()
     {
         if (Input.GetMouseButtonDown(0))
@@ -22,6 +24,17 @@ public class EnableDisable : MonoBehaviour
             //sr.enabled = true;
             //script.enabled = true;
             go.SetActive(true);
+            audioSource.PlayOneShot(clip);
+        }
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            if(audioSource.isPlaying == false)
+            {
+                //audioSource.Play();
+                //audioSource.PlayOneShot(clip);
+            }
+
         }
     }
 }
